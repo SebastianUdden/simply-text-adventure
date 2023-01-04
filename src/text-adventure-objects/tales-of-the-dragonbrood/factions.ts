d0ee226cc66db22228f27076e397de4911d7b193
Sebastian Uddén <sebastian.udden@gmail.com>
2023-01-04 14:35:48 +0100

    bo'reth dominion

--- src/text-adventure-objects/tales-of-the-dragonbrood/factions.ts
+++ src/text-adventure-objects/tales-of-the-dragonbrood/factions.ts
@@ -11,4 +11,12 @@ export const FACTIONS = `Gor’dal
 Okana
 * They live secluded on the island of Sindar, beyond the Great Plains in the Western sea
 * Little is known about them aside from the hired mercenaries and assassins that are rumoured to be from the island. These are rare but most likely spotted in one of the Warcamps deep in the Shadowlands jungle 
-* The Okana of the Sindar island occasionally strike out in Domestication Enclaves, but these enclaves are seldom reported as they will kill and burn anything they don’t bring with them as slaves`;
+* The Okana of the Sindar island occasionally strike out in Domestication Enclaves, but these enclaves are seldom reported as they will kill and burn anything they don’t bring with them as slaves
+
+Bo’reth Dominion
+* The Bo’reth Dominion is located at the largest intersection of the two inner seas.
+* It’s neighboured by the Fire isles in the north and the Smoldering peaks and Shadowlands jungle to the south
+* They’ve invested heavily in earth tech, domesticating many earth-drakes found in the Smouldering peaks and beyond. These have been used to erect a huge dam protecting the inner sea from the regular tsunamis of the boiling sea, thus giving it the name Lake of Serenity.
+* Their capital of Bo’r is built into and around the dam (simply known as Bo), since it generates huge amount of energy, which they use to power their innovative machinery
+* The Bo’reth Dominion controls a quite small landmass compared to the other factions, but they’ve made up for this with a high population density and multi-story buildings.
+* Building high structures is only possible for the few who can tame earth-drakes, but even if they can be created the main issue for using it as housing lies in the energy requirements of transporting food and other living essentials up and down the buildings. This is something that the Bo’reth has solved by their dependency on Bo and the recurring tsunamis of the boiling sea.`;
