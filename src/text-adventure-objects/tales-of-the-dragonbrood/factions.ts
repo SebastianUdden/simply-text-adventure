83a20bc22df643d79922ab347108eee91e41be05
Sebastian Uddén <sebastian.udden@gmail.com>
2023-01-04 15:49:51 +0100

    lodur, organath, em'reth

--- src/text-adventure-objects/tales-of-the-dragonbrood/factions.ts
+++ src/text-adventure-objects/tales-of-the-dragonbrood/factions.ts
@@ -1,4 +1,4 @@
-export const FACTIONS = `Gor’dal
+export const FACTIONS = `Gor’dal Empire
 * A mighty underground empire, seldom interacting with the surface dwellers. 
 * Gor’dal made its wealth by building a tunnel network between the western and eastern highlands, thus connecting the two continents below the Northern pass and allowing for large amounts of trade to flow through it. 
 * Home to Doro
@@ -19,4 +19,21 @@ Bo’reth Dominion
 * They’ve invested heavily in earth tech, domesticating many earth-drakes found in the Smouldering peaks and beyond. These have been used to erect a huge dam protecting the inner sea from the regular tsunamis of the boiling sea, thus giving it the name Lake of Serenity.
 * Their capital of Bo’r is built into and around the dam (simply known as Bo), since it generates huge amount of energy, which they use to power their innovative machinery
 * The Bo’reth Dominion controls a quite small landmass compared to the other factions, but they’ve made up for this with a high population density and multi-story buildings.
-* Building high structures is only possible for the few who can tame earth-drakes, but even if they can be created the main issue for using it as housing lies in the energy requirements of transporting food and other living essentials up and down the buildings. This is something that the Bo’reth has solved by their dependency on Bo and the recurring tsunamis of the boiling sea.`;
+* Building very tall structures is only possible for the few who can tame earth-drakes, but even if they can be created the main issue for using for housing lies in the energy requirements of transporting food and other living essentials up and down the buildings. This is something that the Bo’reth has solved through their dependency on Bo and the recurring tsunamis of the boiling sea.
+
+Kingdom of Lodur
+* The Kingdom of Lodur is located north of the lake of serenity and has protective mountains to the west and north as well as access to the Lodur delta in the east. These geographic traits has created a stable kingdom with a focus on tradition and staunch supporter of humanitarian values.
+* Since the royal decree banning trade of machines many centuries ago they’ve kept an overall antagonistic outlook on new technology, instead focusing on traditional magic use and the balance of life.
+* All citizens of Lodur are treated well under their king, with the exception of those leaving the kingdom without a royal decree, as they are forever banned from re-entering. 
+* Since Lodur has remained peaceful for many centuries and it’s resistance to new technology, there are many quiet farming villages where the only major stir is the occasional water-drake sighting or bards who have snuck across the border to sing tales of of the south. 
+
+Organath
+* Organath is essentially a melting pot of all other factions, with liberal trade systems and a constant shifting of governing factions, so the people here have learned to rely on themselves.
+* Merchants wield huge powers in what essentially works as city states, commonly known as the Orgo, across Organath. The Orgo have little to no interaction with the northern Kingdom of Lodur because of their ban on machines and strict border rules.
+* The Orgo, being essentially self-governed, are in a perpetual state of war and peace with each other and the surrounding factions. This is especially true because of Em’reth, the powerful southern empire who continually manipulates the Orgo into different power structures in order to keep them out of their own borders.
+
+Em’reth
+* The Em’reth, roughly translated as ‘the people of the righteous wind’, are a people living in the southeastern part of Dalar. 
+* They use magic and technology equally, and are ruled by an authoritarian regime using a combination of surveillance and military power to keep any resistance in check. 
+* They also wage continuous war on other factions, especially with some of the Orgo to the north and the Bo’reth of the west, as a means to move focus away from internal strife.
+`;
